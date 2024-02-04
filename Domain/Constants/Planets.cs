f61ee1bae11b348e4fb18a37bd8320b21b66231e
Roma Tariq <roma.imran@gmail.com>
2024-02-04 15:15:49 +0200

    Add planet constants

---
+++ Domain/Constants/Planets.cs
@@ -0,0 +1,13 @@
+namespace Domain.Constants;
+
+public static class Planets
+{
+    public const string Mercury = "Mercury";
+    public const string Venus = "Venus";
+    public const string Earth = "Earth";
+    public const string Mars = "Mars";
+    public const string Jupiter = "Jupiter";
+    public const string Saturn = "Saturn";
+    public const string Uranus = "Uranus";
+    public const string Neptune = "Neptune";
+}
